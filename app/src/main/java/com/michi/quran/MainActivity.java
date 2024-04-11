a576a3a1d584421d95198eb1b813a193fd20186b
briant24 <briantadr24@gmail.com>
2024-04-12 01:13:45 +0700

    Setup fragment for feature

--- app/src/main/java/com/michi/quran/MainActivity.java
+++ app/src/main/java/com/michi/quran/MainActivity.java
@@ -1,14 +1,33 @@
 package com.michi.quran;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private final HomeFragment homeFragment = new HomeFragment();
+    private final QiblaFragment qiblaFragment = new QiblaFragment();
+    private final BookmarkFragment bookmarkFragment = new BookmarkFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.menu_home){
+                selectedFragment = homeFragment;
+            } else if (item.getItemId() == R.id.menu_qibla) {
+                selectedFragment = qiblaFragment;
+            } else if (item.getItemId() == R.id.menu_bookmark) {
+                selectedFragment = bookmarkFragment;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
     }
 }
